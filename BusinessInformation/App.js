1408009fe33956645c9dd4c69738aea01233239e
evan <evanaaronswood@gmail.com>
2024-01-19 14:32:57 -0500

    Updated main file to show my information

--- BusinessInformation/App.js
+++ BusinessInformation/App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View, Image, Linking } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.imagecontainer}>
+          <Image
+            style={styles.image}
+            source={require("./assets/images/image0.jpeg")}
+          />
+        </View>
+        <View style={styles.linkscontainer}>
+          <View style={styles.textWrapper}>
+            <Text style={styles.text}>Evan Aarons-Wood</Text>
+          </View>
+          <View style={styles.textWrapper}>
+            <Text style={styles.text} onPress={() => Linking.openURL("mail:evanaaronswood@gmail.com")}>evanaaronswood@gmail.com</Text>
+          </View>
+          <View style={styles.textWrapper}>
+            <Text style={styles.text} onPress={() => Linking.openURL("tel:+18432314126")}>(843) 231-4126</Text>
+          </View>
+          <View style={styles.textWrapper}>
+            <Text style={styles.text} onPress={() => Linking.openURL("https://github.com/vs0t")}>My Github</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#9CAF88",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imagecontainer: {
+    flex: 3,
+    justifyContent: 'center', // Add this line
+    alignItems: 'center', // Add this line
+    // backgroundColor: "#0a3504",
+  },
+  image: {
+    height: 450,
+    width: 350,
+    resizeMode: "contain", // Change this to 'contain' to avoid cropping
+    alignSelf: 'center', // Add this line
+  },
+  linkscontainer: {
+    flex: 2,
+    justifyContent: "space-evenly", // Change this to space-evenly
+    alignItems: "center",
+    backgroundColor: "#0f4b07",
+    borderWidth: 3,
+    borderColor: 'black',
+    width: "70%",
+    marginBottom: 50,
+  },
+  textWrapper: {
+    borderWidth: 1, // Set your desired border width
+    borderColor: 'black', // Set your desired border color
+    padding: 10, // Add padding to make it look better
+    width: '80%', // Set width as per your design preference
+    marginBottom: 10, // Add margin at the bottom
+  },
+  text: {
+    fontSize: 15,
+    textAlign: 'center', // Add this line to center text
   },
 });
