25a8299a60fe81160885e6c62029420d52f67c6b
evan <evanaaronswood@gmail.com>
2024-01-29 12:33:30 -0500

    added list of actual 8 ball responses from d2l

--- Week 3/EightBall/App.js
+++ Week 3/EightBall/App.js
@@ -25,14 +25,26 @@ export default function App() {
   const handlePress = () => {
     // array of possible magic 8 ball responses
     const responses = [
-      "Yes",
-      "No",
-      "Maybe",
-      "Hard to tell",
-      "Absolutely",
-      "Not in a million years",
-      "Try again",
+      "It is certain",
+      "It is decidedly so",
       "Without a doubt",
+      "Yes definitely",
+      "You may rely on it",
+      "As I see it, yes",
+      "Most likely",
+      "Outlook good",
+      "Yes",
+      "Signs point to yes",
+      "Reply hazy, try again",
+      "Ask again later",
+      "Better not tell you now",
+      "Cannot predict now",
+      "Concentrate and ask again",
+      "Don't count on it",
+      "My reply is no",
+      "My sources say no",
+      "Outlook not so good",
+      "Very doubtful"
     ];
     // selecting a random response from the array
     const randomResponse =
